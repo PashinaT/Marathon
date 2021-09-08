263d9b18d3144b8c442fb2a404e855bb0a4af22c
pashinaT <pashinatania@mail.ru>
2021-09-09 00:29:03 +0400

    Marathon time 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Layout from './components/Layout'
+import Footer from './components/Footer'
+
+import bg1 from './assets/bg1.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!!!
-        </p>
-      </header>
+        <Header title={"Марафонный заголовок!"} descr={"Прикольно атрибутики внутри создавать"} />
+        <Layout title={"Слой 1"} urlBg={<img src={bg1} alt="Lobg1go" />}  />
+        <Layout title={"Слой 2"} colorBg={'red'}/>
+        <Layout title={"Слой 3"} urlBg={<img src={bg1} alt="Lobg1go" />}/>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Layout/index.js
@@ -0,0 +1,29 @@
+import s from "./style.module.css"
+import bg1 from "../../assets/bg1.jpg"
+
+const Layout =({title,descr, urlBg, colorBg})=>{
+    console.log(colorBg);
+    console.log(urlBg);
+
+    const stylesd ={
+        background: colorBg,
+        backgroundImage: <bg1/>
+    };
+    return(
+        <section className={s.root} style={stylesd} >
+            <div className={s.wrapper}>
+                <article>
+                    <div className={s.title}>
+                        <h3>{title}</h3>
+                        <span className={s.separator}/>
+                    </div>
+                    <div className={[s.desc, s.full]}>
+                        <p>{descr}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+};
+
+export default Layout
