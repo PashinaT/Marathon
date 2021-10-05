b75fa24f910036d38202258fb9751fdbd9580148
pashinaT <pashinatania@mail.ru>
2021-10-05 21:10:39 +0400

    homework -11

--- src/components/MenuHeader/Navbar/index.js
+++ src/components/MenuHeader/Navbar/index.js
@@ -2,14 +2,19 @@ import s from "./style.module.css"
 import cn from 'classnames'
 import {ReactComponent as LoginSVG} from "../../../assets/login.svg";
 import {ReactComponent as UserSVG} from "../../../assets/user.svg";
-import {useSelector} from "react-redux";
-import {selectUserLoading, selectUserLocalId} from "../../../store/users";
+import {useDispatch, useSelector} from "react-redux";
+import {selectUserLoading, selectUserLocalId, removeUser} from "../../../store/users";
 import {Link} from 'react-router-dom'
 
 const Navbar =({handleClick, onClickLogin,isActive, bgActive=false})=>{
 
     const isLoadingUser = useSelector(selectUserLoading);
     const localId = useSelector(selectUserLocalId);
+    const dispatch =useDispatch();
+
+    const onClickLogOut = () =>{
+        dispatch(removeUser())
+    };
 
     return(
         <nav className={cn(s.root,{[s.bgActive]:bgActive})}>
@@ -27,6 +32,11 @@ const Navbar =({handleClick, onClickLogin,isActive, bgActive=false})=>{
                     <Link className={s.loginWrap} to={'/login'}>
                         <UserSVG/>
                     </Link>
+                    }
+                    {(!isLoadingUser && localId) &&
+                    <div className={s.loginWrap} onClick={onClickLogOut}>
+                        <LoginSVG/>
+                    </div>
                     }
                     <div onClick={handleClick} className={cn(s.menuButton,{ [s.active]:isActive})}>
                         <span/>
